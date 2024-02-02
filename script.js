7be502fcf3d1b416fa5b32cbc118e7e61ed4e951
Peter Tolstrup <petertolstrup@yahoo.com>
2024-02-02 10:14:48 +0100

    template added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Let go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+//const beerURL = "https://kea-alt-del.dk/t7/api/products";
+
+function init() {
+  console.log("init");
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
