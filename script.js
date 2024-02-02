35d3946eba54c0f8c0bb1043ab06c4ca58fd74ba
Peter Tolstrup <petertolstrup@yahoo.com>
2024-02-02 10:27:47 +0100

    cleaning up

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://api.punkapi.com/v2/beers";
-//const beerURL = "https://kea-alt-del.dk/t7/api/products";
 
 function init() {
   console.log("init");
