1bbb04bbb893acc5c0ea2c75742c6460d5e94b20
Peter Tolstrup <petertolstrup@yahoo.com>
2024-02-02 11:04:43 +0100

    A card added dynamically

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ window.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://api.punkapi.com/v2/beers";
 
+let beerTemplate;
+let beerContainer;
+
 function init() {
   console.log("init");
+
+  beerTemplate = document.querySelector(".beer_template");
+  console.log("beerTemplate", beerTemplate);
+
+  beerContainer = document.querySelector(".beer_container");
+  console.log("beer_container", beerContainer);
 
   fetch(beerURL)
     .then(function (response) {
@@ -16,4 +25,12 @@ function init() {
 
 function showBeers(beerJSON) {
   console.log("Første json element", beerJSON[0]);
+
+  let beerClone = beerTemplate.cloneNode(true).content;
+  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
+  beerClone.querySelector(".beer_image").alt = `Picture of a ${beerJSON[0].name} beer`;
+  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
+  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
+  beerClone.querySelector(".beer_description").textContent = beerJSON[0].description;
+  beerContainer.appendChild(beerClone);
 }
